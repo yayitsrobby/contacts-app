4038c5ec33c0953e4bcb319d4bb8f4f767863c71
Robby Kim <robert.st.kim@gmail.com>
2016-06-17 12:21:57 -0400

    html and prototyping js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/*
+Allows user to create and store contacts that include all that
+--Prototype the main contacts object and
+--object.create() when they click add button
+
+--When adding contact, existing contacts should show at bottom
+
+--When existing contacts are selected, it should display on the right side
+
+--Minimum information:
+----First Name, Last Name, Phone Number
+*/
+
+/*------------------ MAIN CODE BODY ------------------*/
+$(document).ready(function () {
+
+  /*------------------ FUNCTIONS ------------------*/
+
+  function addContact() {
+
+  }
+
+  function displayContact() {
+
+  }
+
+  function checkRequirements() {
+
+  }
+
+  /*------------------ VARIABLES ------------------*/
+
+  var contactModel = {
+    first: '',
+    last: '',
+    phone: '',
+    street: '',
+    city: '',
+    state: ''
+  }
+
+})
