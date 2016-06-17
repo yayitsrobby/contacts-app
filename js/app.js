ff812bce1b313a30e323866a1e18f19b3b736cdc
Sean Jun <seanjun21@gmail.com>
2016-06-17 13:57:29 -0700

    Added all the minimum required features

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ Allows user to create and store contacts that include all that
 --When existing contacts are selected, it should display on the right side
 
 --Minimum information:
-----First Name, Last Name, Phone Number
+----First Name, Last Name, Phone Number, State
 */
 
 /*------------------ MAIN CODE BODY ------------------*/
@@ -30,14 +30,29 @@ $(document).ready(function () {
       .append('<li value="' + contactNumber + '"><a href=#' + '>' + newContact.first + ' ' + newContact.last + '</a></li>')
 
     contactNumber++;
+    $('input[type="text"]').val(null);
   }
 
   function displayContact(contact) {
-    console.log(contactList[contact]);
+    var address = "";
+    $('.selected-contact h2').text(contactList[contact].first + ' ' + contactList[contact].last);
+    $('.first-name').text("First Name: " + contactList[contact].first);
+    $('.last-name').text("Last Name: " + contactList[contact].last);
+    $('.phone-number').text("Phone Number: " + contactList[contact].phone);
+
+    if (contactList[contact].street !== '') {
+        address = address + contactList[contact].street + ', ';
+    }
+    if (contactList[contact].city !== '') {
+        address = address + contactList[contact].city + ', ';
+    }
+
+    $('.address-list').html('<li>' + address + contactList[contact].state + '</li>');
+    $('.selected-contact').show();
   }
 
   function isValid() {
-    // CHECK IF THREE INPUTS ARE FILLED IN -- FIRST, LAST, PHONE
+    // CHECK IF FOUR INPUTS ARE FILLED IN -- FIRST, LAST, PHONE
     if (!($('.input-first').val())) {
       return false;
     }
@@ -45,6 +60,9 @@ $(document).ready(function () {
       return false;
     }
     if (!($('.input-phone').val())) {
+      return false;
+    }
+    if (!($('.input-state').val())) {
       return false;
     }
     return true;
@@ -77,10 +95,12 @@ $(document).ready(function () {
 
       //ADD CONTACT
       addContact();
-
+      $('.is-required').text('');
+      
     } else {
 
       // ADD ASTERISK AND ASK THE USER TO FILL-IN
+      $('.is-required').text(' *Required');
     }
 
   });
