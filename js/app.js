eb0e156a9e74196fdc96842cdfe31d6209d10e4c
Sean Jun <seanjun21@gmail.com>
2016-06-17 11:47:26 -0700

    Modified addContact function

--- js/app.js
+++ js/app.js
@@ -17,33 +17,54 @@ $(document).ready(function () {
   /*------------------ FUNCTIONS ------------------*/
 
   function addContact() {
-    
+    var newContact = Object.create(contactModel);
+
+    for (var i = 0 ; i < inputList.length ; i++) {
+        var inputValue = '.input-' + inputList[i];
+
+        newContact[inputList[i]] = $(inputValue).val();
+    }
+    contactList.push(newContact);
+
+    $('.contacts-book').show();
+    $('.contact-list').append('<li>' + )
   }
 
   function displayContact() {
 
   }
 
-  function checkRequirements() {
+  function isValid() {
     // CHECK IF THREE INPUTS ARE FILLED IN -- FIRST, LAST, PHONE
-    if ( !($('.input-first').val()) ) {
-        console.log("True");
-        return true
-    } else {
-        return false
-    }
+    if (!($('.input-first').val())) {
+        console.log('first');
+        return false;
+    } 
+    if (!($('.input-last').val())) {
+        console.log('last');        
+        return false;
+    } 
+    if (!($('.input-phone').val())) {
+        console.log('phone');
+        return false;
+    } 
+    console.log('true');
+    return true;
   }
 
   /*------------------ VARIABLES ------------------*/
 
   var contactModel = {
-    first: '',
-    last: '',
-    phone: '',
-    street: '',
-    city: '',
-    state: ''
-  }
+    first: 'John',
+    last: 'Doe',
+    phone: '111-1111',
+    street: '42 Wallaby Way',
+    city: 'Sydney',
+    state: 'NSW'
+  };
+
+  var contactList = [];
+  var inputList = ['first', 'last', 'phone', 'street', 'city', 'state'];
 
     /*----------------- CODE BODY -----------------*/
 
@@ -51,8 +72,9 @@ $(document).ready(function () {
 
     $('.add-button').click(function() {
         console.log('added');
+
         // IF CHECK REQUIREMENTS === TRUE
-        if ( checkRequirements() ) {
+        if ( isValid() ) {
 
             //ADD CONTACT
             addContact();
